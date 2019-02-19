c38c367a7b60e7ec5e934e7117e35a3da008aeb9
msevimli <info@lucumedia.se>
2019-02-19 18:06:55 +0100

    Wordpress REST API patch
    This plugin is patching wordpress wp-json/wp/v2/users backdoor

---
+++ inc/patch-wp-json.php
@@ -0,0 +1,42 @@
+<?php
+if( ! defined("ABSPATH") ) {
+    exit;
+}
+if(class_exists("patchWPJson")) {
+    die;
+} else {
+    class patchWPJson
+    {
+        public function __construct()
+        {
+            /*
+             *  Thanks to lowtechsun from M for following filters and actions
+            */
+            remove_action('wp_head', 'rest_output_link_wp_head', 10);
+            // You can require authentication for all REST API requests by adding an is_user_logged_in check to the rest_authentication_errors filter.
+            add_filter('rest_authentication_errors', function ($result) {
+                if (!empty($result)) {
+                    return $result;
+                }
+                if (!is_user_logged_in()) {
+                    return new WP_Error('rest_not_logged_in', 'Only authenticated users can access the REST API.', array('status' => 401));
+                }
+                return $result;
+            });
+
+            if (!is_admin()) {
+                // default URL format
+                if (preg_match('/author=([0-9]*)/i', $_SERVER['QUERY_STRING'])) die();
+                add_filter('redirect_canonical', array($this, 'shapeSpace_check_enum'), 10, 2);
+            }
+        }
+
+        function shapeSpace_check_enum($redirect, $request)
+        {
+            // permalink URL format
+            if (preg_match('/\?author=([0-9]*)(\/*)/i', $request)) die();
+            else return $redirect;
+        }
+    }
+    new patchWPJson();
+}
---
+++ patch-boot.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Patch wp-json/users Backdoor
+ * Plugin URI: http://plife.se
+ * Description: This plugin is patching for availability of users data by REST API  wp-json
+ * Version: 1.0
+ * Author: M.Sevimli
+ * Author URI: http://plife.se
+ * License: GPL2
+ */
+
+/* wp-json/wp/v2/users */
+// If you don't want to install this plugin, you can just include  following class directly into the your theme
+// or your custom management plugin
+
+require_once("inc/patch-wp-json.php");
